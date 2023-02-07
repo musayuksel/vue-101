2c3651320f7d66563eb80e9434c773934c6a0586
Musa <79163385+musayuksel@users.noreply.github.com>
2023-02-07 08:35:51 +0000

    Add conditional rendering

--- UsingFromCDN/app.js
+++ UsingFromCDN/app.js
@@ -2,11 +2,17 @@ const app = Vue.createApp({
   //data and functions
   //   template: ' <h2>TEMPLATE</h2>',
   data: () => {
-    return { firstName: 'Musa', lastName: 'Yuxel', age: 33 };
+    return {
+      firstName: 'Musa',
+      lastName: 'Yuxel',
+      age: 33,
+      isOlder: false,
+    };
   },
   methods: {
     increaseNumber() {
       this.age++;
+      this.isOlder = this.age > 35;
     },
   },
 });
