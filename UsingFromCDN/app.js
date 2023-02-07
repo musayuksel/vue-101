34e015d037b4e69d16c272627184633d2ee344b7
Musa <79163385+musayuksel@users.noreply.github.com>
2023-02-07 08:15:02 +0000

    Return data object

--- UsingFromCDN/app.js
+++ UsingFromCDN/app.js
@@ -1,5 +1,8 @@
 const app = Vue.createApp({
   //data and functions
-  template: ' <h2>TEMPLATE</h2>',
+  //   template: ' <h2>TEMPLATE</h2>',
+  data: () => {
+    return { title: 'Hello' };
+  },
 });
 app.mount('#app');
