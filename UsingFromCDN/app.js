2bcde9e185e2d6fb34561e8ceee7da4d9d839e0c
Musa <79163385+musayuksel@users.noreply.github.com>
2023-02-07 08:28:29 +0000

    Add methods and events

--- UsingFromCDN/app.js
+++ UsingFromCDN/app.js
@@ -2,7 +2,12 @@ const app = Vue.createApp({
   //data and functions
   //   template: ' <h2>TEMPLATE</h2>',
   data: () => {
-    return { firstName: 'Musa', lastName: 'Yuxel' };
+    return { firstName: 'Musa', lastName: 'Yuxel', age: 33 };
+  },
+  methods: {
+    increaseNumber() {
+      this.age++;
+    },
   },
 });
 app.mount('#app');
