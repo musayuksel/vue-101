fdcbfa52759ce9999d745ab5a2f275c7dcf75990
Musa <79163385+musayuksel@users.noreply.github.com>
2023-02-08 09:00:01 +0000

    Template with CLI

---
+++ UsingCLI/vue-project/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  
+  <header>
+    <div class="wrapper">
+      <HelloWorld msg="Hello Vue!" />
+    </div>
+  </header>
+</template>
+
+<style scoped>
+
+</style>
